dbfa2a66bfd409b884c7ce3a110672321e5ba2f1
wuding <netjoin@gmail.com>
2019-04-04 08:45:00 +0800

    URL to Path，下载远程音乐文件到本机前缀后对应的文件

--- dist/php/function/file.php
+++ dist/php/function/file.php
@@ -1,4 +1,18 @@
 <?php
+# include 'E:\www\work\netjoin\mr-fact\vendor\wuding\php-ext\src\Filesystem.php';
+include 'E:\www\work\wuding\php-ext\src\Filesystem.php';
+$url = $_GET['url'] ?? 'http://win.web.nf03.sycdn.kuwo.cn/6a03cd58ad212e8863b4756253d14de8/5c9b623e/resource/a3/66/92/195353300.aac';
+
+
+$filesystem = new Ext\Filesystem();
+$data = Ext\Filesystem::getContents($url);
+
+$url = preg_replace(['/:/', '/\/+/'], ['', '/'], $url);
+$url = 'E:/www/legend/dist/' . $url;
+echo Ext\Filesystem::putContents($url, $data, 'not overwrite');
+# print_r($filesystem::$constants);
+exit;
+
 $url = 'https://m.tb.cn/h.3gF2Zlo';
 $file = 'temp/' . md5($url) . '.txt';
 if (file_exists($file)) {
